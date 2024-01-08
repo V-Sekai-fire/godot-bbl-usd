656ef81c0afd05f748539e95e3371e12e74c82dc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-01-08 00:53:05 -0800

    Update build.sh to disable building imaging module

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ jammy main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 # sudo apt-get update
 # bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-# sudo apt install libclang-dev libboost-all-dev libosd-dev cmake libtbb2-dev libclang-17-dev
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DPXR_ENABLE_PYTHON_SUPPORT=FALSE -DPXR_ENABLE_GL_SUPPORT=FALSE -DPXR_ENABLE_METAL_SUPPORT=FALSE -DPXR_ENABLE_VULKAN_SUPPORT=FALSE -DPXR_ENABLE_OSL_SUPPORT=FALSE -DPXR_BUILD_DOCUMENTATION=FALSE -DPXR_BUILD_USDVIEW=FALSE
+# sudo apt install libclang-dev libboost-all-dev libosd-dev cmake libtbb2-dev libclang-17-dev libspdlog-dev
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DPXR_ENABLE_PYTHON_SUPPORT=FALSE -DPXR_ENABLE_GL_SUPPORT=FALSE -DPXR_ENABLE_METAL_SUPPORT=FALSE -DPXR_ENABLE_VULKAN_SUPPORT=FALSE -DPXR_ENABLE_OSL_SUPPORT=FALSE -DPXR_BUILD_DOCUMENTATION=FALSE -DPXR_BUILD_USDVIEW=FALSE -DPXR_BUILD_IMAGING=FALSE
 cmake --build build --config Release
